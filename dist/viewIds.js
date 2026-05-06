117114d544c572536d98d89b32d7a9046196aacb
Nowhere_X <haobot@foxmail.com>
2026-05-06 23:54:06 +0800

    chore(repo): 从 git 追踪中移除 dist 编译输出目录

--- dist/viewIds.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ARDUFLUX_EDITOR_VIEW_ID = void 0;
-exports.ARDUFLUX_EDITOR_VIEW_ID = "arduflux.editor";
-//# sourceMappingURL=viewIds.js.map
